8896315c9e847ce982ef0be612f93bb227f01e67
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-07-05 18:58:57 -0400

    timing my code

---
+++ Code/timingMyCode.js
@@ -0,0 +1,27 @@
+//slower
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+// shows the performance timing
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+
+//faster
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+
+//comparing code which would run faster
