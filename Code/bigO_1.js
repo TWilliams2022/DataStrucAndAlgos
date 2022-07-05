a05368fd8293d33ea8e69cf7c55081d484bf60b3
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-07-05 18:47:48 -0400

    practicing big 0 concepts

---
+++ Code/bigO_1.js
@@ -0,0 +1,19 @@
+function addUpTo(n){
+    let total = 0;
+    for (let i = 1; i <= n ; i++) {
+    total += i;
+    }
+   return total;
+}
+
+console.log(addUpTo(6))
+
+//equivalent to
+
+function addUpTo(m) {
+    return m * (m + 1) /2
+
+}
+
+console.log(addUpTo(6))
+
